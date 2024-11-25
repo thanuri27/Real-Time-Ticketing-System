1ff0115bb00e73a1962fb84195fad2a9a0bb1a2b
thanuri27 <tthamaradini@gmail.com>
2024-11-25 14:40:53 +0530

    Initialized springboot project successfully

---
+++ TicketEventingSystem/src/main/java/com/iitcw/TicketHub/TicketEventingSystem/TicketEventingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.iitcw.TicketHub.TicketEventingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketEventingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketEventingSystemApplication.class, args);
+	}
+
+}
---
+++ TicketEventingSystem/src/test/java/com/iitcw/TicketHub/TicketEventingSystem/TicketEventingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iitcw.TicketHub.TicketEventingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketEventingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
